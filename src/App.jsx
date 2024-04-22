304c2d1c47e3df77f5c9c3da26a008a16ddb655a
ercabsalias <ercabsalias@gmail.com>
2024-04-22 14:07:17 +0100

    Subindo o projecto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Post } from "./Post";
+
+export function App() {
+  return (
+    <>
+      <Post
+        author="Ernesto Salias"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+        Accusantium, possimus deleniti facere modi, ab laudantium fugiat voluptat
+        um natus ipsum, quo suscipit enim earum quas necess
+        itatibus incidunt eveniet aliquam animi hic."
+      />
+      <Post author="Gabril sdsdsd" content="Novo post" />
+    </>
+  );
+}
